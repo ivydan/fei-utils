252c8735ab49c33ce136b8a7559de2b9c6c7ef23
Ju DanDan <ddju@best-inc.com>
2020-03-11 09:51:56 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # fei-utils
+
+创建library
+
+用户已经加载好lodash
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'fei-utils.js',
+        library: 'feiUtils',
+        libraryTarget: 'umd'
+    },
+    externals: {
+        lodash: {
+            commonjs: 'lodash',
+            commonjs2: 'lodash',
+            amd: 'lodash',
+            root: '_'
+        }
+    }
+};
